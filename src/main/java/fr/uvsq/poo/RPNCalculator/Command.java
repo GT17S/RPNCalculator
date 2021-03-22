11737f3ea064ffe74e20aa2212e33b158b116801
GT17S <guenanetoufik0615@gmail.com>
2021-03-22 15:37:25 +0100

    Création de dossier RPNCalculator contenant le fichiers sources, et création de l'interface Command.java

---
+++ src/main/java/fr/uvsq/poo/RPNCalculator/Command.java
@@ -0,0 +1,13 @@
+package fr.uvsq.poo.RPNCalculator;
+
+/**
+ * @author GT17S & Koussaila
+ * Interface fonctionnelle Command
+ */
+
+@FunctionalInterface
+public interface Command {
+
+    public void apply();
+
+}
