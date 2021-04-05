b0f5f164a3d7626079efcdba87f054b679c33f04
GT17S <guenanetoufik0615@gmail.com>
2021-04-05 17:07:43 +0200

    Creation d'une classe permettant d'allumer et eteindre la calculatrice qui permet par la suite de faire un quit et eteindre la calculatrice

---
+++ src/main/java/fr/uvsq/poo/RPNCalculator/Interrupteur.java
@@ -0,0 +1,44 @@
+package fr.uvsq.poo.RPNCalculator;
+
+/**
+ * @author GT17S
+ * Class Interrupteur est classe qui permet d'allumer ou eteindre la calculatrice RPN.
+ */
+
+public class Interrupteur {
+
+    private boolean interrupteurAllumer;
+
+    /**
+     * Constructeur sans parameters
+     */
+    public Interrupteur() {
+        this.interrupteurAllumer = true;
+    }
+
+
+    /**
+     * Methode sans parameters qui verifier si l'interrupteur de la calculatrice est allumer ou pas.
+     */
+    public boolean estAllumer() {
+        return interrupteurAllumer;
+    }
+
+
+    /**
+     * Methode sans parameters qui permet d'allumer l'interrupteur de la calculatrice et allumer cette derniere
+     * par la suite.
+     */
+    public void Allumer() {
+        interrupteurAllumer = true;
+    }
+
+
+    /**
+     * Methode sans parameters qui permet d'eteindre l'interrupteur de la calculatrice et eteindre cette derniere
+     * par la suite.
+     */
+    public void Eteindre() {
+        interrupteurAllumer = false;
+    }
+}
