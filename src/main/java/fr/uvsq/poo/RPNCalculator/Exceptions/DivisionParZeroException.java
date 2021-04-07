0f85af72129c4db79d8ebb02294ecaa6dbee3263
GT17S <guenanetoufik0615@gmail.com>
2021-04-07 18:32:48 +0200

    Ajout de la classe HandlerException qui gere les exceptions sur les erreurs qui pourront naitre a cause des valeurs entrees ou bien la taille de la pile

--- src/main/java/fr/uvsq/poo/RPNCalculator/Exceptions/DivisionParZeroException.java
+++ src/main/java/fr/uvsq/poo/RPNCalculator/Exceptions/DivisionParZeroException.java
@@ -1,6 +1,6 @@
 package fr.uvsq.poo.RPNCalculator.Exceptions;
 /**
- * Excepetion pour le cas d'une devision sur 0
+ * Exception pour le cas d'une devision sur 0
  * @author Koussaila 21805388
  */
 public class DivisionParZeroException extends ArithmeticException {
---
+++ src/main/java/fr/uvsq/poo/RPNCalculator/Exceptions/HandlerException.java
@@ -0,0 +1,24 @@
+package fr.uvsq.poo.RPNCalculator.Exceptions;
+/**
+ * Exception pour gerer la taille de la pile et la valeur maximale autorisé.
+ * @author GT17S
+ */
+
+public class HandlerException {
+
+    private static final double MINIMUM_VALUE = 500000;
+    private static final double MAXIMUM_VALUE = 500000;
+
+    public static void HandleMinMax(double numbre) {
+        if(numbre > MAXIMUM_VALUE || numbre < -MINIMUM_VALUE) {
+            throw new MinMaxValueException();
+        }
+    }
+
+    public static void HandleStackSize(int actualSize, int wantedSize) {
+        if(actualSize < wantedSize) {
+            throw new IllegalArgumentException("Vous avez pas assez d'éléments dans la pile pour cette opération.");
+        }
+    }
+
+}
