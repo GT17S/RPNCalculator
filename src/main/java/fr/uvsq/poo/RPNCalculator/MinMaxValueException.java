e6d8aabf1ac524a378e4e5566b36deefe218c812
koussaila <da700890@gmail.com>
2021-04-07 18:04:31 +0200

    Ajout de la Classe MinMaxValueException

---
+++ src/main/java/fr/uvsq/poo/RPNCalculator/MinMaxValueException.java
@@ -0,0 +1,31 @@
+package fr.uvsq.poo.RPNCalculator;
+
+/**
+ * @author koussaila 21805388
+ * Exception renvoyée quand la  valeur est trop grande
+ */
+
+public class MinMaxValueException extends  ArithmeticException {
+    /**
+     * Constructeur de notre exception
+     */
+    public MinMaxValueException() {
+        super("Valeur  trop grande pour être gérée par la pile ");
+    }
+
+}
+
+
+
+
+
+/
+public class MinMaxException extends ArithmeticException {
+
+
+
+}
+
+
+
+
