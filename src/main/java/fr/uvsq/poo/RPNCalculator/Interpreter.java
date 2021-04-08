a563ef2759e3dbbdfd9796c4d33ceb2d7c3a5205
GT17S <guenanetoufik0615@gmail.com>
2021-04-08 13:42:06 +0200

    Création de la classe Interpreter qui permet d'interpreter les inputs et les sauvegardes dans pile qui vas permettre par la suite de les suppprimer et meme faire un undo, et elle permet aussi de quitter le programme.

---
+++ src/main/java/fr/uvsq/poo/RPNCalculator/Interpreter.java
@@ -0,0 +1,65 @@
+package fr.uvsq.poo.RPNCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author GT17S
+ * Class Interpreter qui interprete les differentes etapes de calcule RPN et qui permet par la suite de les stockées.
+ */
+public class Interpreter{
+
+    private Stack<Undoable> historique;
+    private Interrupteur interrupteur;
+    private List<String> logger;
+
+    public Interpreter() {
+        this.historique = new Stack<Undoable>();
+        this.interrupteur = new Interrupteur();
+        this.logger = new ArrayList<String>();
+    }
+
+    /**
+     * Exécuter une commande (Operation)
+     * @param command
+     */
+    private void applyCommand(Command command) {
+        command.apply();
+    }
+
+    /**
+     * Exécute une commande annulable puis la sauvegarder dans l'historique
+     * La commande n'est pas ajouté dans l'historique si elle lance une exception.
+     * @param command
+     */
+    public void applyStoreCommand(Undoable command) {
+        try {
+            command.apply();
+            this.historique.add(command);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void undo() {
+        this.applyCommand(new Undo(historique));
+    }
+
+    public void log(String string) {
+        this.applyCommand(() -> logger.add(string));
+    }
+
+    public String getLastLog() {
+        return logger.get(logger.size() - 1);
+    }
+
+    public void Eteindre() {
+        this.applyCommand(new Quit(interrupteur));
+    }
+
+    public boolean isOn() {
+        return interrupteur.estAllumer();
+    }
+
+}
